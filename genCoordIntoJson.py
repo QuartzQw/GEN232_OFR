cd406eb388b36b74660a5075d189a7ade392795b
QuartzQw <boonyaritsamran@gmail.com>
2025-01-29 21:24:50 +0700

    feat: add new files for demo

---
+++ genCoordIntoJson.py
@@ -0,0 +1,50 @@
+import aspose.words as aw
+import json
+import io
+
+positionData = []
+doc = aw.Document("./generateDoc/demo.docx")
+
+# Use LayoutCollector and LayoutEnumerator to calculate coordinates of the shapes.
+collector = aw.layout.LayoutCollector(doc)
+enumerator = aw.layout.LayoutEnumerator(doc)
+
+# Get the shapes.
+shapes = doc.get_child_nodes(aw.NodeType.SHAPE, True)
+for s in shapes :
+    shape = s.as_shape()
+    # skip shapes from header/footer.
+    if shape.get_ancestor(aw.NodeType.BODY) == None :
+        continue
+
+    # # process only toplevel
+    # if not shape.is_top_level :
+    #     continue
+        
+    enumerator.set_current(collector, shape)
+    # # Process only the first page.
+    # if enumerator.page_index>1 :
+    #     break
+
+    rect = enumerator.rectangle
+    # print(f"X={rect.left}; Y={rect.top}; Width={rect.width}; heigth={rect.height}")
+    positionData.append([rect.left, rect.top, rect.width, rect.height, enumerator.page_index])
+
+    # open template(.json file)
+with open('./generateDoc/mockupTemplate.json', 'r', encoding="utf-8") as file:
+    jsonContent = json.load(file)
+
+questionIndex = 0
+for question in jsonContent["questions"]:
+    for choice, att in question["choices"].items():
+        att["x"] = positionData[questionIndex][0]
+        att["y"] = positionData[questionIndex][1]
+        att["width"] = positionData[questionIndex][2]
+        att["height"] = positionData[questionIndex][3]
+        att["page"] = positionData[questionIndex][4]
+        questionIndex += 1
+
+jsonText = str(jsonContent).replace('\'', '"')
+
+with io.open("./generateDoc/modifiedMockupTemplate.json", 'w', encoding="utf-8") as file:
+    file.write(jsonText)
---
+++ genDocx.py
@@ -0,0 +1,96 @@
+from docx import Document
+from docx.shared import Inches, Cm, Pt
+from docx.text.run import *
+import json
+
+def create_choice_inform(runChoices ,choice, att):
+    choiceType = att["choiceType"]
+
+    # varies choice behavior for different input area in docx 
+    if choiceType == "checkBox":
+        runChoices.add_picture("./generateDoc/circle.jpg", width=Inches(0.12))
+        runChoices.add_text(f" {choice}")
+        runChoices.add_text("            ")
+    elif choiceType == "checkBoxWithText":
+        runChoices.add_picture("./generateDoc/circle.jpg", width=Inches(0.12))
+        runChoices.add_text(f" {choice} ")
+        runChoices.add_picture("./generateDoc/grayBox.jpg", width=Inches(1), height = Inches(0.25))
+        runChoices.add_text("            ")
+    elif choiceType == "text":
+        runChoices.add_text(f" {choice} ")
+        runChoices.add_picture("./generateDoc/grayBox.jpg", width=Inches(1), height = Inches(0.25))
+        runChoices.add_text("    ")
+    elif choiceType == "longText":
+        runChoices.add_text(f" {choice} ")
+        runChoices.add_picture("./generateDoc/grayBox.jpg", width=Inches(3), height = Inches(0.25))
+        runChoices.add_text("    ")
+    elif choiceType == "number":
+        runChoices.add_text(f" {choice} ")
+        runChoices.add_picture("./generateDoc/grayBox.jpg", width=Inches(1), height = Inches(0.25))
+        runChoices.add_text(f' {att["unit"]}   ')
+    
+
+def create_question_in_form(question):
+
+    # add question text to document
+    questionLine = document.add_paragraph(f'{question["question"]}')
+    questionLine.style.font.size = Pt(16)
+    questionLine.style.font.bold = True
+
+    # prepare runs line (list of choices)
+    parChoices = document.add_paragraph()
+    runChoices = parChoices.add_run()
+    runChoices.font.size = Pt(14)
+    runChoices.font.bold = False
+
+    # for every choice and their attributes
+    for choice,att in question["choices"].items():
+        create_choice_inform(runChoices ,choice, att)
+        
+
+# main code
+
+document = Document()
+document.styles["Normal"].font.name = "Angsana New"
+
+# set up page structures (border width)
+sections = document.sections
+for sec in sections:
+    sec.top_margin = Cm(0.5)
+    sec.bottom_margin = Cm(0.5)
+    sec.left_margin = Cm(1)
+    sec.right_margin = Cm(1)
+
+# handle unknow heading
+headingList = []
+
+# open template(.json file)
+with open('./generateDoc/mockupTemplate.json', 'r', encoding="utf-8") as file:
+    jsonContent = json.load(file)
+
+# extract section and sub-section data
+sectionProperties = jsonContent["sectionProperties"]
+
+# for every question in template file
+for question in jsonContent["questions"]:
+
+    # extract section index
+    sectionNo = question["section"]
+    subSectionNo = question["subSection"]
+    questionType = question["type"]
+    section = sectionProperties[sectionNo-1]
+    sectionName = section["sectionName"]
+    subSection = section["subSection"][subSectionNo - 1]
+    
+    # if found the question that is in unknow section, create new section 
+    if sectionNo not in headingList:
+        headingList.append(sectionNo)
+        document.add_heading(f"ส่วนที่ {sectionNo}: {sectionName}", level=0)
+
+    # add question to the form, behaviour may varies from type of question
+    create_question_in_form(question)
+
+
+document.save('./generateDoc/demo.docx')
+
+
