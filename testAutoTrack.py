223db2843ffbeab5c01d85c38166cd6a83d440de
QuartzQw <boonyaritsamran@gmail.com>
2025-02-09 14:25:06 +0700

    feat: auto detection over track page, reduce hough circle detection

--- testAutoTrack.py
+++ testAutoTrack.py
@@ -1,62 +1,56 @@
 import cv2 
 import numpy as np 
+import argparse
   
-# Read image. 
-img = cv2.imread('./data/demo3.jpg', cv2.IMREAD_COLOR) 
-# img = cv2.resize(image, (0,0), fx = 1, fy = 1)
+def scan(filePath, **kwargs):
 
-# Convert to grayscale. 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-  
-# Blur using 3 * 3 kernel. 
-gray_blurred = cv2.blur(gray, (3, 3)) 
+    coords = []
 
-# adaptive threshold
-thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,51,6)
-# cv2.imshow("transformed", thresh) 
+    # Read image. 
+    image = cv2.imread(filePath, cv2.IMREAD_COLOR)
+    img = cv2.resize(
+        image, 
+        (kwargs["imgWidth"] * kwargs["resolution"],
+          kwargs["imgHeight"] * kwargs["resolution"]),
+        interpolation = cv2.INTER_AREA)
+    # cv2.imshow("transformed", img) 
 
-# Fill rectangular contours
-cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-for c in cnts:
-    cv2.drawContours(thresh, [c], -1, (255,255,255), -1)
+    # Convert to grayscale. 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 
-# Morph open
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
-opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=4)
+    # adaptive threshold
+    thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,kwargs["blockSize"],kwargs["cVal"])
+    # cv2.imshow("transformed", thresh) 
 
-# Draw rectangles, the 'area_treshold' value was determined empirically
-cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-area_treshold = 4000
-for c in cnts:
-    if cv2.contourArea(c) > area_treshold :
-      x,y,w,h = cv2.boundingRect(c)
-      cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 3)
-  
-# Apply Hough transform on the blurred image. 
-detected_circles = cv2.HoughCircles(
-    gray_blurred,                 
-    cv2.HOUGH_GRADIENT,
-    1, 
-    20, 
-    param1 = 60, 
-    param2 = 40, 
-    minRadius = 0, 
-    maxRadius = 40) 
-  
-# Convert the circle parameters a, b and r to integers. 
-detected_circles = np.uint16(np.around(detected_circles)) 
+    # Fill rectangular contours
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        cv2.drawContours(thresh, [c], -1, (255,255,255), -1)
+    # cv2.imshow("transformed", thresh) 
 
-for pt in detected_circles[0, :]: 
-    a, b, r = pt[0], pt[1], pt[2] 
+    # Morph open
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=4)
 
-    # Draw the circumference of the circle. 
-    cv2.circle(img, (a, b), r, (0, 255, 0), 2) 
+    # Draw rectangles, the 'area_treshold' value was determined empirically
+    cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        if cv2.contourArea(c) > kwargs["minArea"] :
+            x,y,w,h = cv2.boundingRect(c)
+            ratio = w/h
+            if (1.1 >= ratio >= 0.9): 
+                coords.append([x,y,x+w,y+h,"checkBox"])
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255,255,12), 2)
+            elif(ratio >= 1.5):
+                coords.append([x,y,x+w,y+h,"text"])
+                cv2.rectangle(img, (x, y), (x + w, y + h), (36,255,12), 2)
 
-    # Draw a small circle (of radius 1) to show the center. 
-    cv2.circle(img, (a, b), 1, (0, 0, 255), 3) 
-
-image = cv2.resize(img, (0,0), fx = 0.25, fy = 0.25)
-cv2.imshow("Detected Circle", image) 
-cv2.waitKey(0) 
+    
+    img = cv2.resize(
+            img, 
+            (kwargs["imgWidth"], kwargs["imgHeight"]),
+            interpolation = cv2.INTER_AREA)
+    cv2.imshow("Detected Circle", img) 
+    cv2.waitKey(0) 
--- testMouseTk.py
+++ testMouseTk.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk
+from testAutoTrack import scan
 
 ix = -1
 iy = -1
@@ -7,6 +8,7 @@ coords = []
 drawStat = False
 
 app = Tk()
+Scrollbar(app).pack(side = RIGHT, fill= Y)
 width, height = app.winfo_screenwidth(), app.winfo_screenheight()
 winWidth = int(width * 0.8)
 winHeight = height-100
@@ -25,34 +27,45 @@ def get_xy(event):
         drawStat = False
     for rect in coords:
         print(coords)
-        canvas.create_rectangle(rect[0], rect[1], rect[2], rect[3], width = 1)
+        formCanv.create_rectangle(rect[0], rect[1], rect[2], rect[3], width = 1)
 
 def del_element(event):
-    canvas.create_image(10,10, image = image, anchor = "nw")
+    formCanv.create_image(10,10, image = image, anchor = "nw")
     idxToDel = -1
     for i, rect in enumerate(coords):
         if (rect[0] <= event.x <= rect[2] and rect[1] <= event.y <= rect[3]):
             idxToDel = i
         else:
-            canvas.create_rectangle(rect[0], rect[1], rect[2], rect[3], width = 1)
+            formCanv.create_rectangle(rect[0], rect[1], rect[2], rect[3], width = 1)
     del coords[idxToDel]
-                 
 
-canvas = Canvas(app)
-canvas.pack(anchor="nw")
+def autoDetectPress(imgPath, imgWidth, imgHeight):
+    # print(blockSize.get(), cVal.get(), minArea.get())
+    # print(param1.get(), param2.get(), minRad.get(), maxRad.get(), minDist.get())
+    scan(filePath = imgPath,
+         imgHeight = imgHeight,
+         imgWidth = imgWidth,
+         blockSize = blockSize.get(),
+         cVal = cVal.get(),
+         minArea = minArea.get(),
+         resolution = resolution.get())
 
-canvas.bind("<Button-1>", get_xy)
-canvas.bind("<Button-3>", del_element)
+formCanv = Canvas(app)
+formCanv.pack(anchor="nw")
 
-img = Image.open("./data/demo3.jpg")
+formCanv.bind("<Button-1>", get_xy)
+formCanv.bind("<Button-3>", del_element)
+
+imgPath = "./data/demo3.jpg"
+img = Image.open(imgPath)
 scaleFactor = formHeight/img.height
 formWidth = int(img.width * scaleFactor)
 formHeight = int(img.height * scaleFactor)
 
-canvas.config(width=formWidth, height= formHeight)
+formCanv.config(width=formWidth, height= formHeight)
 image = img.resize((formWidth, formHeight))
 image = ImageTk.PhotoImage(image)
-canvas.create_image(10,10, image = image, anchor = "nw")
+formCanv.create_image(10,10, image = image, anchor = "nw")
 
 # w = Canvas(app, width=200, height=200)
 # w.create_rectangle(0,0,50,50)
@@ -64,29 +77,23 @@ Label(app, text="Parameter tuning for auto-detect").place(x = xAuto, y = yAuto)
 Label(app, text="block size").place(x = xAuto+20, y = yAuto+40)
 Label(app, text="C-value").place(x = xAuto+20, y = yAuto+80)
 Label(app, text="min. area").place(x = xAuto+20, y = yAuto+120)
-btnFind = Button(app, text="Auto detect")
-btnFind.place(x = xAuto, y = yAuto + 180)
+Label(app, text="resolution").place(x = xAuto+20, y = yAuto+160)
 blockSize = Scale(app, from_=3, to=255, length = scaleLength, resolution=2, orient=HORIZONTAL)
 blockSize.place(x=xAuto + 80, y= yAuto+20)
 cVal = Scale(app, from_=1, to=15, length = scaleLength, resolution=2, orient=HORIZONTAL)
 cVal.place(x=xAuto + 80, y= yAuto+60)
-minArea = Scale(app, from_=100, to=10000, length = scaleLength, orient=HORIZONTAL)
+minArea = Scale(app, from_=0, to=1000, length = scaleLength, orient=HORIZONTAL)
 minArea.place(x=xAuto + 80, y= yAuto+100)
+resolution = Scale(app, from_=1, to=8, length = scaleLength, orient=HORIZONTAL)
+resolution.place(x=xAuto + 80, y= yAuto+140)
 
-Label(app, text="param1").place(x = xAuto+280, y = yAuto+40)
-Label(app, text="param2").place(x = xAuto+280, y = yAuto+80)
-Label(app, text="min. Rad.").place(x = xAuto+280, y = yAuto+120)
-Label(app, text="max. Rad.").place(x = xAuto+280, y = yAuto+160)
-Label(app, text="min. Dist.").place(x = xAuto+280, y = yAuto+200)
-param1 = Scale(app, from_=20, to=200, length = scaleLength, resolution=5, orient=HORIZONTAL)
-param1.place(x=xAuto + 340, y= yAuto+20)
-param2 = Scale(app, from_=20, to=200, length = scaleLength, resolution=5, orient=HORIZONTAL)
-param2.place(x=xAuto + 340, y= yAuto+60)
-minRad = Scale(app, from_= 0, to=500, length = scaleLength, resolution=5, orient=HORIZONTAL)
-minRad.place(x=xAuto + 340, y= yAuto+100)
-maxRad = Scale(app, from_= 0, to=1000, length = scaleLength, resolution=5, orient=HORIZONTAL)
-maxRad.place(x=xAuto + 340, y= yAuto+140)
-minDist = Scale(app, from_= 0, to=1000, length = scaleLength, resolution=5, orient=HORIZONTAL)
-minDist.place(x=xAuto + 340, y= yAuto+180)
+blockSize.set(141)
+cVal.set(7)
+minArea.set(100)
+resolution.set(2)
+
+autoDetect = Button(app, text="Auto detect", command=lambda:autoDetectPress(imgPath=imgPath, imgWidth=formWidth, imgHeight= formHeight))
+autoDetect.place(x = xAuto, y = yAuto + 180)
 
 app.mainloop()
+
