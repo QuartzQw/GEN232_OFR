e871b5a7bc8971c018b458d534403cfc62c84ebe
QuartzQw <boonyaritsamran@gmail.com>
2025-04-18 20:17:26 +0700

    fix: reorder excel columns

--- saveToExcel.py
+++ saveToExcel.py
@@ -166,7 +166,7 @@ def process_survey(image_folder, templateDir, output_file, image_save_folder):
         os.makedirs("./tempArea")
 
     with open(templateDir, "rb") as f:
-        realCoords = pickle.load(f)
+        realCoords = pickle.load(f)[::-1]
     
     image_paths = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.lower().endswith(('.jpg', '.png', '.jpeg'))]
     all_data = []
